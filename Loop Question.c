b056c130975c13ae6633635e20c2f5052331dd25
Amishi2524 <78739125+Amishi2524@users.noreply.github.com>
2021-02-19 23:10:50 +0530

    Add files via upload

---
+++ Loop Question.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned long long i,j,result=0;
+    for (i=0;i<6;++i)
+    {
+       result=result*100+01;
+       printf("0%llu\n",result);
+    }
+    return 0;
+}
+
